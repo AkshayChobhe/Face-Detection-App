764e272697e6adc6f8031167534e83c1c5339f2b
AkshayChobhe <chobheakshay30@gmail.com>
2023-02-26 11:25:38 +0530

    Face Detection app

---
+++ app/src/main/java/com/example/android/facedetection/FaceDetection.java
@@ -0,0 +1,18 @@
+package com.example.android.facedetection;
+
+import android.app.Application;
+//import android.media.FaceDetector;
+
+import com.google.firebase.FirebaseApp;
+//import com.google.mlkit.vision.face.FaceDetectorOptions;
+
+public class FaceDetection extends Application {
+    public static final String RESULT_TEXT = "RESULT_TEXT";
+    public static final String RESULT_DIALOG = "RESULT_DIALOG";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseApp.initializeApp(this);
+    }
+}
