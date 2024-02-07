6a8ab709d294d5d41931625e82e34501ff15146c
AkshayChobhe <chobheakshay30@gmail.com>
2024-02-07 11:40:08 +0530

    Updated

--- app/src/main/java/com/example/android/facedetection/ResultDialog.java
+++ app/src/main/java/com/example/android/facedetection/ResultDialog.java
@@ -11,6 +11,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+//import org.jetbrains.annotations.Nullable;
+
 public class ResultDialog extends DialogFragment {
     Button okButton;
     TextView resultTV;
